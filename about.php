977333262a08f019c0e8da313581059a7379bea4
Patrick Hanus <patrick@skymse.com>
2016-11-29 21:34:34 -0500

    INITIALIZE Github Repo

---
+++ about.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<title>Welch | Title and Abstract</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Bentham" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Josefin+Sans:400,600&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/icomoon.css" media="screen" />
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-88258809-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+	
+</head>
+
+<body>
+
+<header id="about" class="row">
+
+	<nav id="mainmenu" role="navigation">
+			<a href="index.php">
+			<div class="logo col-3">
+		</div>
+		</a>
+		<div id="menu-burger"><i class="icon-menu"></i></div>
+		<div id="searchicon">		
+		</div>
+		<div id="menu">
+			<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="services.php">Services</a></li>
+			<li><a href="about.php">About</a></li>
+			<li><a href="#contact">Contact</a></li>
+		</ul>
+		</div>
+	</nav>
+
+
+
+	<div class="callout row row-centered">
+		<h1>About</h1>
+		<div class="header-buttons">
+			<a class="button-quote" href="#contact">Get a Quote
+		</a>
+	</div>
+
+	<div class="bottom-arrow"></div>
+
+
+</header>
+
+<content>
+	
+	<div class="section-1 row">
+		
+		<div class="profile col-5">
+			
+		</div>
+
+		<div class="item col-6">
+			<h1>Nick Welch, President</h1>
+			<h2>History</h2>
+					<p>Nick Welch began his career in the title industry at age 19. Working in the Illinois Basin oil industry, Nick was mentored by oil industry legends C.E. Brehm and Marvin Craddock. To this day, he still applies the tools of the trade he learned in the early years of his career.</p><br>
+					
+					<p>Throughout his 37 years in the title business, Nick has worked in nearly every title industry possible. His experience stretches across the coal, oil, gas, wireless telecom, fiber-optic right-of -way, commercial real estate, and residential real estate industries.</p> <br>
+					
+					<p>Over the course of his career, Nick has closed over 85 million dollars of commercial transactions, acquired over 10,000 acres of minerals for coal, oil, and gas exploration; and performed title curative work on over 100 miles of fiber-optic right-of-way.</p>  <br>
+					
+					<p>In addition to his title experience, Nick also has extensive experience with land acquisition. Throughout his career he has acquired 400 cell sites, acquired over 50 miles of fiber-optic right-of-way, and posted a 99%+ success rating in over 150 zoning hearings.</p> <br>
+					
+					<p>Nick’s experience spans 27 different states.</p><br>
+			<h2>Family</h2>
+					<p>Nick Welch lives in Greenwood, Indiana. He and his wife, Anne, have 7 children and 7 grand-children. In his free time, Nick is an avid guitarist. He has a band with 3 of his sons, performing at festivals and events in Indianapolis. According to Nick, his greatest achievement is his family. 
+				</p>
+			
+		</div>
+		
+	</div>
+
+
+
+
+	<div class="section-4 row row-centered"><!-- BEGIN SECTION 4 -->
+		<div class="box col-4 col-m-6">
+            <h2>Here is How to Take Action!</h2>
+            <p>Whether you’re looking for a quote, a professional opinion, or just wanting to share your thoughts, feel free to get in touch! We do our absolute best to reply as soon as we can.</p>
+        </div>
+
+		<div class="box col-4 col-m-6">
+			
+			<form class="form-horizontal" role="form" method="post" action="#contact">
+
+			<?php echo "<p class='text-danger'>$errName</p>";?>
+			<div class="form-group">
+				<label for="name" class="control-label">Name</label>
+					<input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+			</div>
+
+			<?php echo "<p class='text-danger'>$errEmail</p>";?>
+			<div class="form-group">
+				<label for="email" class="contol-label">Email</label>
+					<input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+
+			</div>
+
+			<?php echo "<p class='text-danger'>$errMessage</p>";?>
+			<div class="form-group">
+				<label for="message" class="control-label">How Can We Help?</label>
+					<textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+			</div>
+
+			<?php echo "<p class='text-danger'>$errHuman</p>";?>
+			<div class="form-group">
+					<label for="human" class="control-label">2 + 3 = ?</label>
+					<input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+			</div>
+
+			<div class="form-group">
+					<input id="submit" name="submit" type="submit" value="Get in Touch!" class="btn btn-primary">
+			</div>
+
+			<div class="form-group">
+					<?php echo $result; ?>	
+			</div>
+			
+			</form> 
+
+		</div>
+		
+	</div><!-- END SECTION 4 -->
+
+</content>
+
+<footer>
+	<div id="contact" class="box row">
+		 <img src="images/welch-title_welch-title-square-white.png" />
+		<div class="contact">
+            <div class="contact-info">
+                Nick Welch
+            </div>
+            <div class="contact-info clearfix">
+               (317) 496-0923
+            </div>
+             <a class="contact-info" href="mailto:nick@welch-title.com">
+               nick@welch-title.com
+            </a>
+
+		</div>
+           
+        <div class="social-icons col-3 col-m-12">
+			<li><a href="https://www.facebook.com/profile.php?id=100011194487094" class="social-icon"> <i class="fa fa-facebook"></i></a></li>
+			<li><a href="https://twitter.com/THEOTOKOSRocks" class="social-icon"> <i class="fa fa-twitter"></i></a></li>
+			<li><a href="https://www.linkedin.com/in/nicholas-n-welch-00486213" class="social-icon"> <i class="fa fa-linkedin"></i></a></li>
+		
+		<div class="copyright">
+			Copyright 2016 Welch Title and Abstract
+		</div>
+
+		</div>
+
+		
+	</div>
+
+
+</footer>
+
+<script src="js/jquery.min.js"></script> 
+<script src="js/retina.min.js"></script> 
+<!-- include Masonry -->
+<script src="js/isotope.pkgd.min.js"></script> 
+<!-- include image popups -->
+<script src="js/jquery.magnific-popup.min.js"></script> 
+<!-- include mousewheel plugins -->
+<script src="js/jquery.mousewheel.min.js"></script>
+<!-- include carousel plugins -->
+<script src="js/jquery.tinycarousel.min.js"></script>
+<!-- include svg line drawing plugin -->
+<script src="js/jquery.lazylinepainter.min.js"></script>
+
+
+<!-- include custom script -->
+<script src="js/scripts.js"></script>
+
+</body>
+</html>
