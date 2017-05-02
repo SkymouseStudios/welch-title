70ef59fa69efe2dd5ee26eb260524ea2828ffbc1
Patrick Hanus <patrick@skymse.com>
2017-05-01 21:47:09 -0400

    Converted the Site to Jekyll
    Boom.

---
+++ _layouts/default.php
@@ -0,0 +1,92 @@
+{% incude header.html %}
+
+<content>
+	
+	<div class="section-1 row row-centered">
+		<h2>Every Detail. Exposed and Explained.</h2>
+			<p class="col-5">
+				Welch Abstract and Title provides searches, examinations, and closings on an as-needed basis to help you manage high-volume workloads, drive repeat business, and resolve complex title issues. 
+			</p>
+		<div class="box">
+			<div class="item col-3 col-m-6">
+				<img src="images/icons/Welch-title-icons-04.svg">
+				<h3>Searches</h3>
+				<p>
+					We extract all records relating to a parcel, ensuring you make the best-informed decision backed by all relevant information regarding property or persons involved. 
+				</p>
+			</div>
+			<div class="item col-3 col-m-6">
+				<img src="images/icons/Welch-title-icons-02.svg">
+				<h3>Examinations</h3>
+				<p>
+					We investigate and analyze searched records to determine any obstacles that may block or void your intended use of the property. 
+				</p>
+			</div>
+			<div class="item col-3 col-m-6">
+				<img src="images/icons/Welch-title-icons-06.svg">
+				<h3>Closings</h3>
+				<p>
+					We provide a professional and personable face for your company to finalize transactions on a high-note, driving repeat business from your clients and increasing your reputation for title industry best practices.
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="section-2 row row-centered"><!-- BEGIN SECTION 2 -->
+		<h2>Qualities That Make the Difference.</h2>
+		<div class="box clearfix">
+			<div class="item col-4 col-m-10 col-centered clearfix">
+				<img src="images/icons/Welch-title-icons-01.svg">
+				<h3>Accuracy</h3>
+				<p>Title work is useless to you if it isn’t accurate. <strong>We know that details are important, but even more important is knowing where to look.</strong> Awareness of information outliers can determine the viability of your project before it even gets started, preventing wasted time and money.</p>
+			</div>
+			<div class="item col-4 col-m-10 col-centered clearfix">
+				<img src="images/icons/Welch-title-icons-03.svg">
+				<h3>Knowlgedge</h3>
+				<p>Built on experience: With over 37 years of professional title experience, we offer knowledge drawn from countless projects across multiple title specialties. If 10,000 hours is the time required to be an expert, imagine where 76,960 hours gets you.</p>
+			</div>
+			<div class="item col-4 col-m-10 col-centered clearfix">
+				<img src="images/icons/Welch-title-icons-05.svg">
+				<h3>Contract Based</h3>
+				<p>By offering services on a need-to-work basis, we provide you with the flexibility to handle needs as they arise and to take on projects that only temporarily increase your workload. It also means that you when you win, we win.</p>
+			</div>
+		</div>
+	</div><!-- END SECTION 2 -->
+
+		
+
+	<div class="section-3 row row-centered"><!-- BEGIN SECTION 3 -->
+		
+		<h2>37 Years of Trusted Experience. Take a look.</h2>
+
+		<div class="box clearfix">
+		<div class="col-1"></div>
+			<div class="item-1 col-3 col-m-10 col-centered">
+				<p>"Nick Welch is one of the most trustworthy people that I know, both personally and professionally. He always met my company's needs in a timely manner, while providing excellent customer service through a very personalized approach.”</p>
+				<div class="client">- Derek Phillips, Vice President</div> 
+				<div class="company">Commercial Banking at Centier Bank</div>
+                <!-- <div class="button case-study" href="#">Read the Case Study</div> -->
+			</div>
+			
+            
+			<div class="item-1 col-3 col-m-10 col-centered">
+				<p>"Nick provides consistently reliable results, and is always timely. He's my 'go to' title guy.” </p>
+				<div class="client">Melissa DeGroff, Workout and Loan Documentation Attorney</div> 
+				<div class="company"></div>	
+                <!-- <div class="button case-study" href="#">Read the Case Study</div> -->
+			</div>
+                
+            
+			<div class="item-1 col-3 col-m-10 col-centered">
+				<p>"I've worked with Nick Welch in the past and he was an experienced, trustworthy service provider that quarterbacked the successful closing of numerous building purchase transactions. He delivered great results!”</p>
+				<div class="client">- Scott Lindenberg, Commercial Real-Estate Advisor</div> 
+				<div class="company"></div>	
+                <!-- <div class="button case-study" href="#">Read the Case Study</div> -->
+			</div>	
+			<div class="col-1"></div>
+		</div>
+	</div><!-- END SECTION 3 -->
+
+</content>
+
+{% incude header.html %}
